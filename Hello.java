40279e0a337abe8093865b4a76124d3faab73bbf
Anonymous <Anonymous@JourneyLabss-MacBook-Air-2.local>
2019-05-09 12:26:03 +0530

    Checking conflits in master

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
         public static void main(String[] args)
         {
-            System.out.println("Hello World...........");
+            System.out.println("I am at MASTER BRANCH>..........Hello World...........");
 			System.out.println("Git Learning+master!!!");
 			System.out.println("Yeee, covered git commands with stash");
 
