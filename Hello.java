ef9a9015076ce3e7b2ea8836eb2444d6ef770624
Anonymous <Anonymous@JourneyLabss-MacBook-Air-2.local>
2019-05-09 12:15:57 +0530

    Add Hello world program to other branch

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
         public static void main(String[] args)
         {
-            System.out.println("Hello World");
+            System.out.println("Hello World...........");
 			System.out.println("Git Learning+master!!!");
 			System.out.println("Yeee, covered git commands with stash");
 
